303a0c6fa8f21508093b46a9502668b141a99b06
sletteon <misi.g126@gmail.com>
2017-12-20 14:57:01 +0100

    számok egymillióig, 7-tel osztható számok

--- main.cpp
+++ main.cpp
@@ -3,9 +3,12 @@
 using namespace std;
 int main()
 {
-    for(int i=1;i<=1000;i++)
-        if (i % 3 == 0){
+    for(int i=1;i<=1000000;i++)
+	{
+		if (i%7 == 0)
+		{
         cout<<i<<"\n";
-        }
+		}
+	}
     return 0;
 }
