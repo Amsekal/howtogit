5b3f8cd0767f6193bfe340548cb4b809d59d19b9
András Bogár-Szabó <bszandras@gmail.com>
2017-12-20 15:06:43 +0100

    palindrom

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
+#include <string>
+#include <sstream>
 
 using namespace std;
 int main()
 {
-
-    for(int i=1;i<=1000;i++)
-        if (i % 3 == 0){
-        cout<<i<<"\n";
+    for(int i=1;i<=1000;i++){
+        int Number = i;
+        string String = static_cast<ostringstream*>( &(ostringstream() << Number) )->str();
+        if(String== string( String.rbegin(), String.rend())){
+            cout<<i<<endl;
         }
+    }
 
     return 0;
 }
